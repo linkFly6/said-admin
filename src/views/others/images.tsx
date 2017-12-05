7982814f9fdd94c5bfd70634fedcfb801805f449
Lianming.Wang <liam.wang@cootek.cn>
2017-12-05 22:23:23 +0800

    drag drop

--- src/views/others/images.tsx
+++ src/views/others/images.tsx
@@ -1,25 +1,75 @@
 import * as React from 'react'
-import { Card, Layout } from 'antd'
+import { Card, Layout, message } from 'antd'
 import * as s from './index.styl'
 
-export interface StateProps {
+interface StateProps {
+}
+
+interface PrevState {
+  images: string[]
+}
+
+interface EventTarget1 {
+  target: {
+    result: string
+  }
 }
 
 export default class ImageManager extends React.Component<StateProps> {
+  state = {
+    images: ['http://t2.hddhhn.com/uploads/tu/201607/176/efqomc0rcmu.jpg']
+  }
+
+  componentDidMount() {
+    document.addEventListener('dragover', this.handleDragOver)
+  }
+
+  handleDropdown = e => {
+    const fileList = e.dataTransfer.files
+    if (fileList.length === 0) {
+      return
+    }
+    if (!fileList[0].type.includes('image')) {
+      message.error('拖图片！')
+    }
+    const filePrevs = []
+    const reader = new FileReader()
+    reader.readAsDataURL(fileList[0])
+    reader.onload = (event) => {
+      // console.log(event.target.result as any)
+    }
+    this.setState((prevState: PrevState) => [...prevState.images, fileList[0]])
+    e.stopPropagation()
+    e.preventDefault()
+    return false
+  }
+
+  handleDragOver = e => {
+    document.addEventListener('drop', this.handleDropdown)
+    this.setState({ showDragOverModal: true })
+    e.stopPropagation()
+    e.preventDefault()
+    return false
+  }
+
+  renderImageList = () => {
+    const { images } = this.state
+    return images.map((i, index) => (
+      <Card.Grid className={s.cardGrid} key={index}>
+        <div className={s.gridImage}>
+          <img src={i} alt={i} />
+        </div>
+      </Card.Grid>
+    ))
+  }
+
   render() {
     return (
       <Layout>
-        <Card noHovering title="图片管理" bodyStyle={{ padding: 0 }}>
-          <Card.Grid className={s.cardGrid}>
-            <div className={s.gridImage}>
-              <img src="http://t2.hddhhn.com/uploads/tu/201607/176/efqomc0rcmu.jpg" />
-            </div>
-            <div>
-              <h3>name: 冰山</h3>
-              <p><a href="http://t2.hddhhn.com/uploads/tu/201607/176/efqomc0rcmu.jpg">url</a></p>
-            </div>
-          </Card.Grid>
+        <Card hoverable={false} title="图片管理" bodyStyle={{ padding: 0 }}>
+          {this.renderImageList()}
         </Card>
+
       </Layout>
     )
   }
