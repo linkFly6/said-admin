8d26bc981289751f12c012f7c4f9208cebd26715
Lianming.Wang <liam.wang@cootek.cn>
2017-12-13 21:57:57 +0800

    antd-fileuploader

--- src/views/others/images.tsx
+++ src/views/others/images.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Card, Layout, message } from 'antd'
+import { Card, Icon, Modal, Upload } from 'antd'
 import * as s from './index.styl'
 
 interface StateProps {
@@ -11,62 +11,53 @@ interface PrevState {
 
 export default class ImageManager extends React.Component<StateProps> {
   state = {
-    images: ['http://t2.hddhhn.com/uploads/tu/201607/176/efqomc0rcmu.jpg']
+    images: ['http://t2.hddhhn.com/uploads/tu/201607/176/efqomc0rcmu.jpg'],
+
+    previewVisible: false,
+    previewImage  : '',
+    fileList      : [{
+      uid : -1,
+      size: 999,
+      type: 'done',
+      name: 'xxx.png',
+      url : 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+    }]
   }
 
-  componentDidMount() {
-    document.addEventListener('dragover', this.handleDragOver)
-  }
+  handleCancel = () => this.setState({ previewVisible: false })
 
-  handleDropdown = e => {
-    const fileList = e.dataTransfer.files
-    if (fileList.length === 0) {
-      return
-    }
-    this.previewImages(fileList)
-    e.stopPropagation()
-    e.preventDefault()
-    return false
-  }
-
-  previewImages = files => {
-    Array.from(files as any[]).filter(file => file.type.includes('image')).forEach(img => {
-      const reader = new FileReader()
-      reader.readAsDataURL(img)
-      reader.onload = e => {
-        const target = e.target as any
-        const { result } = target
-        this.setState((prevState: PrevState) => ({images: [...prevState.images, result]}) )
-      }
+  handlePreview = (file) => {
+    this.setState({
+      previewImage  : file.url || file.thumbUrl,
+      previewVisible: true,
     })
   }
 
-  handleDragOver = e => {
-    document.addEventListener('drop', this.handleDropdown)
-    this.setState({ showDragOverModal: true })
-    e.stopPropagation()
-    e.preventDefault()
-    return false
-  }
-
-  renderImageList = () => {
-    const { images } = this.state
-    return images.map((i, index) => (
-      <Card.Grid className={s.cardGrid} key={index}>
-        <div className={s.gridImage}>
-          <img src={i} alt={i} />
-        </div>
-      </Card.Grid>
-    ))
-  }
+  handleChange = ({ fileList }) => this.setState({ fileList })
 
   render() {
+    const { previewVisible, previewImage, fileList } = this.state
+    const uploadButton = (
+      <div>
+        <Icon type="plus" />
+        <div className="ant-upload-text">Upload</div>
+      </div>
+    )
     return (
-      <Layout>
-        <Card hoverable={false} title="图片管理" bodyStyle={{ padding: 0 }}>
-          {this.renderImageList()}
-        </Card>
-      </Layout>
+      <div className="clearfix">
+        <Upload
+          action="//jsonplaceholder.typicode.com/posts/"
+          listType="picture-card"
+          fileList={fileList}
+          onPreview={this.handlePreview}
+          onChange={this.handleChange}
+        >
+          {fileList.length >= 3 ? null : uploadButton}
+        </Upload>
+        <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
+          <img alt="example" style={{ width: '100%' }} src={previewImage} />
+        </Modal>
+      </div>
     )
   }
 }
