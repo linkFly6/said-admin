9520b606a6eab962274fd313ba11e7df17b3b4ce
linkFly <linkFly6@live.com>
2018-08-27 18:49:22 +0800

    剔除无用代码、加入包检查

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -348,6 +348,10 @@ module.exports = {
     // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
     // You can remove this if you don't use Moment.js:
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+
+    // ======================== 检查包细节和大小，用于优化 ========================
+    // new (require('webpack-bundle-analyzer').BundleAnalyzerPlugin)()
+
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
